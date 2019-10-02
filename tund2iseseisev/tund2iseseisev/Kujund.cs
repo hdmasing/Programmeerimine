3ba4a1083a9df228276cc5893c0c9081a1b3e91b
Hans <hansdietrich.masing@gmail.com>
2019-10-02 12:24:40 +0300

    tunnitöö

---
+++ tund2iseseisev/tund2iseseisev/Kujund.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tund2iseseisev
+{
+    public class Kujund
+    {
+        public virtual int A { get; set; }
+        public virtual int B { get; set; }
+        public class Ristkülik : Kujund
+        {
+            public override int A
+            {
+                get { return Convert.ToInt32(Console.ReadLine()); }
+            }
+            public override int B
+            {
+                get { return Convert.ToInt32(Console.ReadLine());}
+            }
+            public class Ruut : Kujund
+            {
+                public override int A
+                {
+                    get { return Convert.ToInt32(Console.ReadLine()); }
+                }
+                public override int B
+                {
+                    get { return Convert.ToInt32(Console.ReadLine()); }
+                }
+            }  
+        }
+            public int Pindala(int A, int B)
+            {
+                
+                return A * B;
+            }
+    }
+}
---
+++ tund2iseseisev/tund2iseseisev/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tund2iseseisev
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+                     
+            Console.WriteLine("Vali kujund, mida soovid sisestada, 1 on ristkülik ja 0 on ruut");
+            bool answer = p.Yesorno();
+            if (answer == true)
+            {
+                 Kujund.Ristkülik.Ruut ruut = new Kujund.Ristkülik.Ruut();   
+                 Console.Write("Sisesta ruudu külg A");
+                 int c = ruut.A;
+                 int d = c;
+                 Console.WriteLine("Ruudu pindala on "+ruut.Pindala(c, d));
+            }else if (answer == false)
+            {
+                Kujund.Ristkülik ristkülik= new Kujund.Ristkülik();
+                Console.WriteLine("Sisesta ristküliku külg A ja seejärel B");
+                Console.Write("Ristküliku külg A: "); int a = ristkülik.A;
+                Console.Write("Ristküliku külg B: "); int b = ristkülik.B;
+                Console.WriteLine("Ristküliku pindala on " + ristkülik.Pindala(a,b));
+            }
+
+        }
+        private bool Yesorno()
+            {
+                repeatthisquestion:
+                string number = Console.ReadLine();
+                int abi = 2;
+                bool tõene = int.TryParse(number, out abi);
+                if (!tõene)
+                {
+                    Console.WriteLine("Sisesta 1 või 0");
+                    
+                    goto repeatthisquestion;
+                }
+
+                if (abi == 1)
+                {
+                    return false;
+                   
+                }
+                else if (abi == 0)
+                {
+
+                    goto beginning;
+
+                }
+                else
+                {
+                    Console.WriteLine("Palun sisesta 1 või 0");
+                    goto repeatthisquestion;
+                }
+                beginning:
+                return tõene;
+            }
+    }
+}
