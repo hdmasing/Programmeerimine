34ae4b696b13c7093cf7dd8295832ed55856ee72
Reio Reimers <reio.reimers@hotmail.com>
2019-10-09 13:26:44 +0300

    Add

---
+++ ConsoleApp2/ConsoleApp2/Food.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tunnitoo3.Models
+{
+    public class Food
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+       
+
+        public Food(string name, double price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Price = price;
+           
+        }
+
+        public double CalculateSum(Food food, double a)
+        {
+            double sum = 0;
+            sum = food.Price * a;
+            return sum;
+        }
+        
+
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tunnitoo3.Models;
+
+namespace tunnitoo3
+{
+    public class Program
+    {
+      
+        
+        static void Main(string[] args)
+        {
+        List<double> prices  = new List<double>();
+            List<Food> groceries = new List<Food>
+            {
+                new Food("apple", 1.7),
+                new Food("bread", 2),
+                new Food("cheese", 3)
+            };
+            ShoppingCart newCart = new ShoppingCart(0);
+            foreach (var food in groceries)
+            {
+                newCart.Items.Add(food);
+            }
+            beginning:
+            askagain: Console.WriteLine("Millise toidu soovid korvi lisada?");
+            string userinput = Console.ReadLine();
+            Food chosenFood = groceries.FirstOrDefault(x => x.Name == userinput);
+            int a;
+            if (chosenFood == null)
+            {
+                Console.WriteLine("Toodet pole nimekirjas, sisesta teine toode.");
+                goto askagain;//suunab tagasi ,rida 34
+            }
+            else
+            {
+                Console.WriteLine("Kui suurt kogust soovite?");
+                
+                question: string amount = Console.ReadLine();
+                a = 0;
+                bool success = int.TryParse(amount, out a);
+                if (!success)
+                {
+                    Console.WriteLine("Peab olema täisarv, sisesta uuesti");
+
+                    goto question; //suunab reale 43;
+
+                }
+
+            }
+            Console.WriteLine("Kas te tahate midagi veel osta? Vajuta 1, kui soovid ja 0 kui ei soovi");
+            repeatthisquestion: string number = Console.ReadLine();
+            int abi = 2;
+            bool tõene = int.TryParse(number, out abi);
+            if (!tõene)
+            {
+                Console.WriteLine("Sisesta täisarv");
+                goto repeatthisquestion;//suunab tagasi reale 54;
+            }
+
+            if (abi == 1)
+            {
+                double sum = chosenFood.CalculateSum(chosenFood, a);
+                prices.Add(sum);
+                Console.WriteLine("Toote maksumus on " + sum + "€");
+                goto beginning;//tagasi reale 33
+
+            }
+            else if (abi == 0)
+            {
+                double sum = chosenFood.CalculateSum(chosenFood, a);
+                prices.Add(sum);
+                Console.WriteLine("Toote maksumus on " + sum + "€");
+
+            }
+            else
+            {
+                Console.WriteLine("Sisesta kas 1-jah või 0-ei");
+                goto repeatthisquestion;//suunab reale 54
+            }
+            double fullprice=0;
+            foreach (double sum in prices)
+            {
+                
+                fullprice = sum + fullprice;
+                newCart.Sum = fullprice;
+            }
+            using (var db = new ShopDbContext())
+            {
+                db.ShoppingCarts.Add(newCart);
+                db.SaveChanges();
+                var carts = db.ShoppingCarts;
+                
+                foreach (var VARIABLE in carts)
+                {
+                    Console.WriteLine(Convert.ToString(VARIABLE.DateCreated));
+
+                    
+                }
+            }
+            
+        }
+    }
+}
